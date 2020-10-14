4f51c37aacce5bdbe40be0f0df377fda080190d0
NN-Guillaume <neebnicoli.guillaume@gmail.com>
2020-10-14 22:18:10 +0200

    Single listing page is done

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models.expressions import OrderBy
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from listings.models import Listing
+from realtors.models import Realtor
+
 
 def index(request):
-    listings = Listing.order_by('-list_date').filter(is_published=True)[:3]
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
 
     context = {
         'listings': listings
@@ -13,4 +21,15 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
